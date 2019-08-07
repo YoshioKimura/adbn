7c607fd86700345d88719ffcd9780e2fb191760a
gsgitsample <thk.rspt.csd@gamil.com>
2019-08-08 01:03:20 +0900

    エリアに触れただけで変化する場所が明るくなる機能を実装

--- sample.js
+++ sample.js
@@ -1,42 +1,78 @@
 
 $(function(){
 
-var html;
-// $("#btn").on('click', function () {
-
-function render(){
-  var img_position = $("#img_position").val();
-  var zid = $("#zid").val();
-  var color = $("#advertiser_text_color").val();
+  var html;
+  var img_position;
+  var zid;
+  var color; 
 
 
   //frame
-  var frame_border_bold = $("#frame_border_bold").val();
-  var frame_border_color = $("#frame_border_color").val();
-  var frame_background_color = $("#frame_background_color").val();
+  var frame_border_bold;
+  var frame_border_color;
+  var frame_background_color;
   //title
-  var title_color = $("#title_color").val();
-  var title_paddintop = $("#title_paddintop").val();
+  var title_color;
+  var title_paddintop;
 
   //img
-  var img_path = $("#img_path").val();
-  var img_width = $("#img_width").val();
-  var img_height = $("#img_height").val();
-  var img_margin_top = $("#img_margin_top").val();
-  var img_margin_left = $("#img_margin_left").val();
-  var img_margin_right = $("#img_margin_right").val();
-  var img_margin_bottom = $("#img_margin_top").val();
-  var img_border_radius = $("#img_border_radius").val();
-  var img_border_radius_unit = $("#img_border_radius_unit").val();
+  var img_path;
+  var img_width;
+  var img_height;
+  var img_margin_top;
+  var img_margin_left;
+  var img_margin_right;
+  var img_margin_bottom;
+  var img_border_radius;
+  var img_border_radius_unit;
   
   
-  var pr_text_color = $("#pr_text_color").val();
-  var pr_font_size = $("#pr_font_size").val();
-  var pr_position_bottom = $("#pr_position_bottom").val();
-  var pr_position_right = $("#pr_position_right").val();
+  var pr_text_color;
+  var pr_font_size;
+  var pr_position_bottom;
+  var pr_position_right;
+  
+  function render(){
+    zid = $("#zid").val();
+    color = $("#advertiser_text_color").val();
+    img_position = $("#img_position").val();
+  
+  
+  //frame
+  frame_border_bold = $("#frame_border_bold").val();
+  frame_border_color = $("#frame_border_color").val();
+  frame_background_color = $("#frame_background_color").val();
+  //title
+  title_color = $("#title_color").val();
+  title_paddintop = $("#title_paddintop").val();
+
+  //img
+  img_path = $("#img_path").val();
+  img_width = $("#img_width").val();
+  img_height = $("#img_height").val();
+  img_margin_top = $("#img_margin_top").val();
+  img_margin_left = $("#img_margin_left").val();
+  img_margin_right = $("#img_margin_right").val();
+  img_margin_bottom = $("#img_margin_top").val();
+  img_border_radius = $("#img_border_radius").val();
+  img_border_radius_unit = $("#img_border_radius_unit").val();
+  
+  
+  pr_text_color = $("#pr_text_color").val();
+  pr_font_size = $("#pr_font_size").val();
+  pr_position_bottom = $("#pr_position_bottom").val();
+  pr_position_right = $("#pr_position_right").val();
 
 
   html = `<style>
+                  .textcolor-flash{
+                      color:#FF6969 !important;
+                  }
+
+                  .bgcolor-flash{
+                      background:#FF6969 !important;
+                  }
+
                 li {
                   font: inherit;
                   vertical-align: baseline;
@@ -56,7 +92,7 @@ function render(){
                   position: relative !important;
                   margin-bottom: 23px;
                   border:solid ${frame_border_color} ${frame_border_bold}px;
-
+                  background:${frame_background_color};
                 }
             
                 .gn-nad_frst_${zid} a {
@@ -142,6 +178,15 @@ function render(){
 render();
 
 
+$("input").on('focus',function(){
+    // console.log($(this).attr(data));
+    console.log($('iframe').contents().find(`[class=gn-nad_frst_${zid}]`));
+    $('iframe').contents().find(`[class=gn-nad_frst_${zid}]`).addClass('bgcolor-flash').delay(500).queue(function(next){
+      $('iframe').contents().find(`.gn-nad_frst_${zid}`).removeClass('bgcolor-flash');
+      next();  
+    });
+});
+
 $("#reload").on('click', render);
 
 
@@ -157,9 +202,12 @@ $('textarea').focus(function () {
     document.execCommand('copy');
 });
 
-window.document.body.addEventListener('change', (event) => {
+window.document.body.addEventListener('change', () => {
   console.log(123);
+  render();
 });
+
+// $("h2").on('click', render);
 
 
 });
